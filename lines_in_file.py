af93fff2f9346f05ef397fcff776daacaf1a7965
fatehhundal <fshundal@hotmail.com>
2025-06-11 18:31:54 +0800

    lines_in_file

---
+++ lines_in_file.py
@@ -0,0 +1,12 @@
+file = open('new_document 2 2.txt', 'r')
+Counter = 0
+
+Content = file.read()
+CoList = Content.split("\n")
+
+for i in CoList:
+    if i:
+        Counter += 1
+
+print("This is the number of lines in the file.")
+print(Counter)
